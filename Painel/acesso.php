d02ee1165d05f11cb17733b20807c8810e67cc7e
henderson mele <hrmele@gmail.com>
2021-04-23 17:53:40 -0300

    melhorias tela de login

--- Painel/acesso.php
+++ Painel/acesso.php
@@ -6,22 +6,45 @@ if (isset($_SESSION["logado"])) {
         $logado=true;
     }
 }
-if (!$logado) {
-    /*echo '<pre>';
-    print_r($_SERVER);
-    echo '</pre>';*/
-    echo '<form action="'.$_SERVER["REQUEST_SCHEME"].'://'.$_SERVER["HTTP_HOST"].'/Painel/logar.php?origem='.urlencode($_SERVER["REQUEST_SCHEME"].'://'.$_SERVER["HTTP_HOST"].$_SERVER['REQUEST_URI']).'" method="POST">';
-    echo '<label for="login">Email</label>';
-    echo '<input type="text" name="login" id="login" />';
-    echo '<label for="senha">Senha</label>';
-    echo '<input type="password" name="senha" id="senha" />';
-    echo '<input type="submit" value="Enviar"/>';
-    echo '</form>';
-    exit();
+?>
+<?php
+    if (!$logado) {
+        $origem = urlencode($_SERVER["REQUEST_SCHEME"].'://'.$_SERVER["HTTP_HOST"].$_SERVER['REQUEST_URI']);
+        header("Location:".$_SERVER["REQUEST_SCHEME"].'://'.$_SERVER["HTTP_HOST"].'/Painel/logar.php?origem='.$origem);
+/*
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+    	<meta charset="UTF-8">
+    	<title>Livres BS - Consumo Consciente</title>
+    	<link rel="stylesheet" href="../css/foundation.css">
+    	<link href="https://cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.3.26/theme-default.min.css" rel="stylesheet" type="text/css" />
+    	<link rel="stylesheet" href="login.css">
+    	<script>
+    		window.console = window.console || function(t) {};
+    	</script>
+    	<script>
+    		if (document.location.search.match(/type=embed/gi)) {
+    			window.parent.postMessage("resize", "*");
+    		}
+    	</script>
+    </head>
+    <body translate="no">
+    	<div class="container">
+    		<div class="form">
+                <form class="login-form" id="login" name="login" method="POST" action="<?php echo $_SERVER["REQUEST_SCHEME"].'://'.$_SERVER["HTTP_HOST"].'/Painel/logar.php?origem='.urlencode($_SERVER["REQUEST_SCHEME"].'://'.$_SERVER["HTTP_HOST"].$_SERVER['REQUEST_URI']); ?>">
+    			    <input type="hidden" id="tipoL" name="tipo" value="login" />
+    				<p><label for="login">Email</label><input type="text" placeholder="Email" name="login" id="login" /></p>
+    				<p><label for="senha">Senha</label><input type="password" placeholder="Senha" name="senha" id="senha" /></p>
+    				<button>Entrar</button>
+    			</form>
+    		</div>
+    	</div>
+    </body>
+</html>
+*/
+exit();
 } else {
-    if ($_SESSION["level"] == 1000) {
-        //header("Location: ./montagem");
-    }
     if (isset($levelRequired)) {        
         if ($_SESSION["level"] < $levelRequired) {
             echo 'Você não tem permissão de acesso à esta página';
--- Painel/logar.php
+++ Painel/logar.php
@@ -5,11 +5,21 @@ error_reporting(E_ALL);
 
 include "../config.php";
 include "password.php";
+
+session_start();
+
 if (isset($_GET["origem"])) {
 	$origem = urldecode($_GET["origem"]);
 } else {
-	$origem = $_SERVER["HTTP_HOST"]."/Painel";
+	$origem = $_SERVER["REQUEST_SCHEME"].'://'.$_SERVER["HTTP_HOST"]."/Painel";
 }
+
+if (isset($_SESSION["logado"])) {
+    if ($_SESSION["logado"] == "sim") {
+        header("Location: ".$origem);
+    }   
+}
+
 //$origem = $_SERVER["HTTP_REFERER"];
 if (isset($_POST["login"]) && isset($_POST["senha"])) {
     $login= $_POST["login"];
@@ -22,19 +32,58 @@ if (isset($_POST["login"]) && isset($_POST["senha"])) {
         $rs = $st->fetchAll();
         $senhaDB = $rs[0]["password"];
         if (password_verify($senha,$senhaDB)) {
-            session_start();
             $_SESSION["logado"] = "sim";
             $_SESSION["level"] = $rs[0]["level"];
             $_SESSION["login"] = $rs[0]["login"];
             $_SESSION["id"] = $rs[0]["id"];
             header("Location:".$origem);
         } else {
-            echo "Email/Senha incorreto(s).";
+            $msg = "Email/Senha incorreto(s).";
         }
     } else {
-        echo "Email/Senha incorreto(s)!";
+        $msg = "Email/Senha incorreto(s)!";
     }
 } else {
-    echo "Preencha email e senha.";
+    $msg = "Preencha email e senha.";
+}
+if (strlen($msg) > 0) {
+    if (isset($_GET["origem"])) {
+        $origem = $_GET["origem"];
+    } else {
+        $origem = urlencode($_SERVER["REQUEST_SCHEME"].'://'.$_SERVER["HTTP_HOST"].$_SERVER['REQUEST_URI']);
+    }
+?>
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+    	<meta charset="UTF-8">
+    	<title>Livres BS - Consumo Consciente</title>
+    	<link rel="stylesheet" href="../css/foundation.css">
+    	<link href="https://cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.3.26/theme-default.min.css" rel="stylesheet" type="text/css" />
+    	<link rel="stylesheet" href="login.css">
+    	<script>
+    		window.console = window.console || function(t) {};
+    	</script>
+    	<script>
+    		if (document.location.search.match(/type=embed/gi)) {
+    			window.parent.postMessage("resize", "*");
+    		}
+    	</script>
+    </head>
+    <body translate="no">
+    	<div class="container">
+    		<div class="form">
+                <form class="login-form" id="login" name="login" method="POST" action="<?php echo $_SERVER["REQUEST_SCHEME"].'://'.$_SERVER["HTTP_HOST"].'/Painel/logar.php?origem='.$origem; ?>">
+                    
+    			    <input type="hidden" id="tipoL" name="tipo" value="login" />
+    				<p><label for="login">Email</label><input type="text" placeholder="Email" name="login" id="login" /></p>
+    				<p><label for="senha">Senha</label><input type="password" placeholder="Senha" name="senha" id="senha" /></p>
+    				<button>Entrar</button>
+    			</form>
+    		</div>
+    	</div>
+    </body>
+</html>
+<?php
 }
 ?>
