daa059d9f59ee0b26465d94ce076b00f05ead63c
henderson mele <hrmele@gmail.com>
2021-12-29 14:03:03 -0300

    Viabilizar botão 'limpar' data / Acrescentar link de pagamentos

--- Painel/defineDataSessao.php
+++ Painel/defineDataSessao.php
@@ -2,17 +2,26 @@
 require_once "acesso.php";
 require_once "../includes/autoloader.inc.php";
 if (isset($_GET["data"])) {
-    $livres = new Livres;
+    if ($_GET["data"] == "limpar") {
+        if (isset($_SESSION["data_consulta"])) {
+            unset($_SESSION["data_consulta"]);
+        }
+        if (isset($_SESSION["data_id"])) {
+            unset($_SESSION["data_id"]);
+        }
+    } else {
+        $livres = new Livres;
     
-    $data = $_GET["data"];
-    $data = DateTime::createFromFormat('d/m/Y H:i', $data);
-    $data = $data->format('Y-m-d H:i');
-
-    $dataId = $livres->dataPelaString($data);
-
-    $_SESSION['data_consulta'] = $data;
-    $_SESSION['data_id'] = $dataId;
-    echo $_SESSION['data_consulta'];
+        $data = $_GET["data"];
+        $data = DateTime::createFromFormat('d/m/Y H:i', $data);
+        $data = $data->format('Y-m-d H:i');
+    
+        $dataId = $livres->dataPelaString($data);
+    
+        $_SESSION['data_consulta'] = $data;
+        $_SESSION['data_id'] = $dataId;
+        echo $_SESSION['data_consulta'];
+    }
 } else {
     if (isset($_SESSION['data_consulta'])) {
         echo $_SESSION['data_consulta'];
