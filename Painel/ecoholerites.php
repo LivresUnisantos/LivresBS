2353112ba8cfc8a86664605f5359eaa94de36c39
henderson mele <hrmele@gmail.com>
2021-12-30 11:43:20 -0300

    Criado controle de ecoholerites

---
+++ Painel/ecoholerites.php
@@ -0,0 +1,160 @@
+<?php
+$levelRequired=1000;
+require_once "../includes/autoloader.inc.php";
+require_once '../twig/autoload.php';
+require_once "acesso.php";
+
+$livres = new Livres();
+$calendario = new Calendario();
+
+$loader = new \Twig\Loader\FilesystemLoader('../templates/layouts/painel');
+$twig = new \Twig\Environment($loader, ['debug' => false]);//
+
+$filter = new \Twig\TwigFilter('stripslashes', 'stripslashes');
+$twig->addFilter($filter);
+
+$alerta = "";
+$sucesso = "";
+if (isset($_SESSION["alerta"])) { 
+    $alerta = $_SESSION["alerta"];
+    unset($_SESSION["alerta"]);
+};
+if (isset($_SESSION["sucesso"])) {
+    $sucesso = $_SESSION["sucesso"];
+    unset($_SESSION["sucesso"]);
+};
+
+/******
+//Controle de permissão será feito por login individual para mais segurança
+Permissões concedidas:
+id = 1 Henderson | id = 5 Milena
+
+//Caso haja permissão de aprovador, serão listadas as atividades de todas as pessoa
+Para isso a criação do objeto da classe é diferente
+****/
+$permissao_aprovador = false;
+if ($_SESSION["id"] == 1 ||$_SESSION["id"] == 5) {
+    $permissao_aprovador = true;
+    $oEcoholerite = new Ecoholerite();
+} else {
+    $oEcoholerite = new Ecoholerite($_SESSION["id"]);
+}
+
+//Tratar quando há GET (aprovação/reprovação/remoção de atividades)
+/* act = aprovar/reprovar/remover */
+if (isset($_GET["act"])) {
+    $act = $_GET["act"];
+    if ($act == 'aprovar_todos') {
+        if ($oEcoholerite->aprovaAtividadeExecutada()) {
+            $sucesso = "Todas as atividades pendentes foram aprovadas.";
+            $_SESSION["sucesso"] = $sucesso;
+        } else {
+            $alerta = "Erro ao aprovar atividades";
+            $_SESSION["alerta"] = $alerta;
+        }
+    } else {
+        $id_atividade = $_GET["id_atividade"];
+        $atividade = $oEcoholerite->atividadesExecutadas($id_atividade);
+        if (!$atividade) {
+            $alerta = "A atividade que você tentou ".$act." não existe";
+            $_SESSION["alerta"] = $alerta;
+        } else {
+            switch($act) {
+                case "aprovar":
+                    if ($oEcoholerite->aprovaAtividadeExecutada($id_atividade)) {
+                        $sucesso = "Atividade código ".$id_atividade." aprovada";
+                        $_SESSION["sucesso"] = $sucesso;
+                    } else {
+                        $alerta = "Erro ao aprovar atividade";
+                        $_SESSION["alerta"] = $alerta;
+                    }
+                break;
+                case "reprovar":
+                    if ($oEcoholerite->reprovaAtividadeExecutada($id_atividade)) {
+                        $sucesso = "Atividade código ".$id_atividade." reprovada";
+                        $_SESSION["sucesso"] = $sucesso;
+                    } else {
+                        $alerta = "Erro ao reprovar atividade";
+                        $_SESSION["alerta"] = $alerta;
+                    }
+                break;
+                case "remover":
+                    if ($oEcoholerite->removeAtividadeExecutada($id_atividade)) {
+                        $sucesso = "Atividade código ".$id_atividade." ('".$atividade["descricao"]."' de '".$atividade["nome"]."' de '".date('d/m/Y',strtotime($atividade["data"]))."') removida";
+                        $_SESSION["sucesso"] = $sucesso;
+                    } else {
+                        $alerta = "Erro ao remover atividade";
+                        $_SESSION["alerta"] = $alerta;
+                    }
+                break;
+            }
+        }
+    }
+    exit();
+}
+
+//Tratar quando há POST (cadastro de novas atividades)
+if (isset($_POST["add_salvar"])) {
+    $erro = false;
+    $campos = ['add_nome','add_data','add_atividade','add_ecohoras','add_valor','add_comentario'];
+    //checa preenchimento
+    foreach ($campos as $campo) {
+        if (!isset($_POST[$campo])) {
+            $alerta = "Não foi possível cadastrar atividade. Campo ".$campo." não preenchido";
+            $erro=true;
+        } else {
+            $$campo = $_POST[$campo];
+        }
+    }
+    //checa consistência da atividade/usuário selecionado
+    if (!$erro) {
+        $atividade = $oEcoholerite->listaAtividades($add_atividade);
+        if (!$atividade) {
+            $alerta = "Não foi possível cadastrar atividade. Atividade selecionada não encontrada.";
+            $erro = true;
+        }
+        $pessoa = $oEcoholerite->listaPessoas($add_nome);
+        if (!$pessoa) {
+            $alerta = "Não foi possível cadastrar atividade. Nome selecionado não encontrado.";
+            $erro = true;
+        }
+    }
+    //checa consistência da ecohora/valor
+    if (!$erro) {
+        if ($atividade["ecohoras"] != 0 && ($atividade["ecohoras"] != $add_ecohoras || $atividade["valor"] != $add_valor)) {
+            $alerta = "Não foi possível cadastrar atividade. Os dados de ecohora/valor não batem com os dados cadastrados.";
+            $erro = true;
+        }
+    }
+    //tudo certo, cadastrar
+    if (!$erro) {
+        if ($oEcoholerite->addAtividadeExecutada($add_nome, $add_atividade, $add_data, $add_ecohoras, $add_valor, $add_comentario)) {
+            $sucesso = "Atividade cadastrada";
+        } else {
+            $alerta = "Erro ao cadastrar atividade. Tente novamente.";
+        }
+    }
+    $_SESSION["alerta"] = $alerta;
+    $_SESSION["sucesso"] = $sucesso;
+    header("Location: ecoholerites.php");
+}
+
+$conteudo = $oEcoholerite->atividadesExecutadas();
+$atividades = $oEcoholerite->listaAtividades();
+$pessoas = $oEcoholerite->listaPessoas();
+
+echo $twig->render('ecoholerites.html', [
+    "titulo"                => "LivresBS - Ecoholerite",
+    "alerta"                => $alerta,
+    "sucesso"               => $sucesso,
+    "admin_logado"          => $_SESSION["id"],
+    "permissao_aprovador"   => $permissao_aprovador,
+    "readonly"              => (!$permissao_aprovador) ? 'readonly="readonly"' : '', //variável auxiliar para bloquear campos que são editáveis apenas ao aprovador
+    "disabled"              => (!$permissao_aprovador) ? 'disabled="disabled"' : '', //variável auxiliar para bloquear campos que são editáveis apenas ao aprovador
+    "conteudo"              => $conteudo,
+    "atividades"            => $atividades,
+    "pessoas"               => $pessoas,
+    "data_selecionada"      => (isset($_SESSION['data_consulta']) ? date('d/m/Y H:i',strtotime($_SESSION["data_consulta"])) : ""),
+    ]);
+    
+?>
---
+++ classes/Ecoholerite.class.php
@@ -0,0 +1,105 @@
+<?php
+class Ecoholerite extends Livres {
+
+    public $idAdmin;
+
+    function __construct($idAdmin = "") {
+        $this->idAdmin = $idAdmin;
+    }
+
+    public function atividadesExecutadas($id_ecoholerite = "") {
+        $sql = "SELECT eh.id as id_ecoholerite, eh.valor as valor_receber, eh.*, ea.*, ad.* FROM Ecoholerites eh LEFT JOIN Ecoatividades ea";
+        $sql .= " ON eh.id_atividade = ea.id ";
+        $sql .= " LEFT JOIN Admins ad ON eh.id_admin = ad.id";
+        if ($id_ecoholerite != "") {
+            $sql .= " WHERE eh.id = ".$id_ecoholerite;
+            if ($this->idAdmin != "") {
+                $sql .= " AND ad.id = ".$this->idAdmin;
+            }
+        } else {
+            if ($this->idAdmin != "") {
+                $sql .= " WHERE ad.id = ".$this->idAdmin;
+            }
+        }
+        $sql .= " ORDER BY data, status";
+        $st = $this->conn()->prepare($sql);
+        $st->execute();
+
+        if ($st->rowCount() == 0) return false;
+
+        if ($id_ecoholerite != "") {
+            return $st->fetch();
+        } else {
+            return $st->fetchAll();
+        }
+            
+    }
+    
+    public function listaAtividades($id = "") {
+        if ($id == "") {
+            $sql = "SELECT * FROM Ecoatividades ORDER BY descricao";
+        } else {
+            $sql = "SELECT * FROM Ecoatividades WHERE id = ".$id." ORDER BY descricao";
+        }
+        $st = $this->conn()->prepare($sql);
+        $st->execute();
+
+        if ($st->rowCount() == 0) return false;
+
+        $rs = $st->fetchAll();
+        return $rs;
+    }
+    
+    public function listaPessoas($id = "") {
+        if ($id == "") {
+            $sql = "SELECT * FROM Admins ORDER BY nome";
+        } else {
+            $sql = "SELECT * FROM Admins WHERE id = ".$id." ORDER BY nome";
+        }
+        $st = $this->conn()->prepare($sql);
+        $st->execute();
+
+        if ($st->rowCount() == 0) return false;
+
+        $rs = $st->fetchAll();
+        return $rs;
+    }
+    
+    public function addAtividadeExecutada($add_nome, $add_atividade, $add_data, $add_ecohoras, $add_valor, $add_comentario) {
+        
+        $data = DateTime::createFromFormat('d/m/Y H:i', $add_data);
+        $data = $data->format('Y-m-d');
+        
+        $add_valor = str_replace(",",".",$add_valor);
+        $add_comentario = $this->conn()->quote($add_comentario);
+        
+        $sql = "INSERT INTO Ecoholerites(id_admin, id_atividade, data, ecohoras, valor, comentario) VALUES";
+        $sql .= " (".$add_nome.",".$add_atividade.",'".$data."',".$add_ecohoras.",".$add_valor.",".$add_comentario.")";
+        $st = $this->conn()->prepare($sql);
+        
+        return $st->execute();
+    }
+    
+    public function removeAtividadeExecutada($id_atividade_exec) {
+        $sql = "DELETE FROM Ecoholerites WHERE id = ".$id_atividade_exec;
+        $st = $this->conn()->prepare($sql);
+        return $st->execute();
+    }
+    
+    public function aprovaAtividadeExecutada($id_atividade_exec = "") {
+        if ($id_atividade_exec != "") {
+            $sql = "UPDATE Ecoholerites SET status= 2 WHERE id = ".$id_atividade_exec;
+        } else {
+            $sql = "UPDATE Ecoholerites SET status= 2 WHERE status = 0";
+        }
+        $st = $this->conn()->prepare($sql);
+        return $st->execute();
+    }
+    
+    public function reprovaAtividadeExecutada($id_atividade_exec) {
+        $sql = "UPDATE Ecoholerites SET status=1 WHERE id = ".$id_atividade_exec;
+        $st = $this->conn()->prepare($sql);
+        return $st->execute();
+    }
+}
+?>
