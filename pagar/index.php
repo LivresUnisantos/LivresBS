1be3be85cfc4e414a5fff68034f2353f5434f09b
henderson mele <hrmele@gmail.com>
2022-01-25 19:01:04 -0300

    Incluído botão para copiar o código pix

--- pagar/index.php
+++ pagar/index.php
@@ -1,3 +1,8 @@
+<html>
+<head>
+<title>Livres Coop - Pagamento</title>
+</head>
+<body>
 <?php
 if (!isset($_GET)) exit();
 
@@ -21,11 +26,27 @@ if ($pixCopiaCola && $pedido) {
     echo '<br>';
     echo "Você também pode copiar o código abaixo e colar no aplicativo de seu banco<br>";
     echo "<p>";
-    echo '<textarea rows="4" cols="60">';
+    echo '<textarea id="copiacola" name="copiacola" rows="4" cols="60">';
     echo $pixCopiaCola;
     echo '</textarea>';
+    echo '<p><button id="btn_copiacola" name="btn_copiacola">Copiar Código</button></p>';
     echo "</p>";
 } else {
     echo "Link de pagamento/pedido não encontrado";
 }
 ?>
+</body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+<script>
+    $(document).ready(function() {
+        $("#btn_copiacola").on("click", function() {
+            $("#copiacola").select();
+            document.execCommand("copy");
+            alert("Código Copiado")
+        });
+    });
+</script>
+</html>
