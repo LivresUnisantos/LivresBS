374345c1165ef4149d86f68edda6b1269ef32cfa
henderson mele <hrmele@gmail.com>
2022-03-24 19:25:52 -0300

    Impedir reconsolidação de pedidos antigos, exceto para PR/Hend

--- Painel/menu_consolidar.php
+++ Painel/menu_consolidar.php
@@ -22,18 +22,27 @@ if (!isset($_SESSION["data_id"]) || $_SESSION["data_id"] == "") {
         "frequencia_semana" => $calendario->montaDisplayFrequenciaSemana(),
         "alerta"          => "Selecione uma data"
         ]);
-        
 } else {
-    $oConsolidar = new ConsolidarPedidos($_SESSION["data_consulta"]);
-    
-    $contagemConsolidado = $oConsolidar->totalPedidosConsolidadosPorData();
-    
-    echo $twig->render('consolidar.html', [
+    if (strtotime($_SESSION['data_consulta']) < strtotime(date("Y-m-d H:i:s")) && $_SESSION["id"] != 1 && $_SESSION["id"] != 3) {
+        echo $twig->render('consolidar.html', [
         "titulo"            => "LivresBS - Consolidar Entregas",
         "menu_datas"        => $calendario->listaDatas(),
         "data_selecionada"  => (isset($_SESSION['data_consulta']) ? date('d/m/Y H:i',strtotime($_SESSION["data_consulta"])) : ""),
         "frequencia_semana" => $calendario->montaDisplayFrequenciaSemana(),
-        "conteudo"          => $contagemConsolidado
+        "alerta"          => "Você não tem permissão para consolidar um pedido passado"
         ]);
+    } else {
+        $oConsolidar = new ConsolidarPedidos($_SESSION["data_consulta"]);
+        
+        $contagemConsolidado = $oConsolidar->totalPedidosConsolidadosPorData();
+        
+        echo $twig->render('consolidar.html', [
+            "titulo"            => "LivresBS - Consolidar Entregas",
+            "menu_datas"        => $calendario->listaDatas(),
+            "data_selecionada"  => (isset($_SESSION['data_consulta']) ? date('d/m/Y H:i',strtotime($_SESSION["data_consulta"])) : ""),
+            "frequencia_semana" => $calendario->montaDisplayFrequenciaSemana(),
+            "conteudo"          => $contagemConsolidado
+            ]);
+    }
 }
 ?>
